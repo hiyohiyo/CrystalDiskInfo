48dd92d770be0318b1c64555c26dfadc8a3c5b0d
Johan Mattsson <39247600+mjunix@users.noreply.github.com>
2022-02-05 22:27:36 +0100

    Fix uninitialized variable

--- AtaSmart.cpp
+++ AtaSmart.cpp
@@ -10537,7 +10537,7 @@ DWORD CAtaSmart::CheckDiskStatus(DWORD i)
 			else
 			{
 				WORD raw = MAKEWORD(vars[i].Attribute[j].RawValue[0], vars[i].Attribute[j].RawValue[1]);
-				WORD threshold; 
+				WORD threshold = 0; 
 				switch(vars[i].Attribute[j].Id)
 				{
 				case 0x05:
